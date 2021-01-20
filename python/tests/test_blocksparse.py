454b844d815f8b0401723dec17020b617d1248e6
Philippe Tillet <phil@openai.com>
2021-01-20 18:44:52 -0500

    more cleaning

--- python/tests/test_blocksparse.py
+++ python/tests/test_blocksparse.py
@@ -3,25 +3,17 @@ import torch
 import triton as tt
 import pytest
 
-def dense_to_sparse(w, mask, block):
-  Z = w.size(0)
-  ret = torch.empty((Z, mask.sum(), block, block), dtype=w.dtype, device=w.device)
-  nnz = mask.nonzero(as_tuple=False)
-  h, i, j = nnz[:, 0], nnz[:, 1], nnz[:, 2]
-  for zz in range(Z):
-    for idx, (hh, ii, jj) in enumerate(zip(h, i, j)):
-      ret[zz, idx, :, :] = w[zz, hh, ii*block: (ii+1)*block, jj*block: (jj+1)*block]
+def sparsify_tensor(x, mask, block):
+  ret = torch.empty((x.size(0), mask.sum(), block, block), dtype=x.dtype, device=x.device)
+  for idx, (h, i, j) in enumerate(zip(*mask.nonzero(as_tuple=True))):
+    ret[:, idx, :, :] = x[:, h, i*block: (i+1)*block, j*block: (j+1)*block]
   return ret
 
-def sparse_to_dense(w, mask, block, zero = 0):
-  maskedw = w.clone()
-  for bz, wz in enumerate(range(0, w.size(0))):
-    for bh, wh in enumerate(range(0, w.size(1))):
-      for bi, wi in enumerate(range(0, w.size(2), block)):
-        for bj, wj in enumerate(range(0, w.size(3), block)):
-          if mask[bh, bi, bj] == 0:
-            maskedw[wz, wh, wi : wi+block, wj:wj+block] = zero
-  return maskedw
+def mask_tensor(x, mask, block, value = 0):
+  ret = x.clone()
+  for h, i, j in zip(*(mask == 0).nonzero(as_tuple=True)):
+    ret[:, h, i*block: (i+1)*block, j*block: (j+1)*block] = value
+  return ret
 
 @pytest.mark.parametrize("MODE, TRANS_A, TRANS_B, BLOCK", 
     [
@@ -35,24 +27,24 @@ def test_op(MODE, TRANS_A, TRANS_B, BLOCK, DTYPE = torch.float16, Z = 3, H = 2, 
   # set seed
   torch.random.manual_seed(0)
   # create inputs
-  x = torch.randn((Z, H, K, M) if TRANS_A else (Z, H, M, K), dtype=DTYPE, device='cuda')
-  w = torch.randn((Z, H, N, K) if TRANS_B else (Z, H, K, N), dtype=DTYPE, device='cuda')
-  shape = {'sdd': (M, N), 'dsd': (x.shape[2], x.shape[3]), 'dds': (w.shape[2], w.shape[3])}[MODE]
+  a = torch.randn((Z, H, K, M) if TRANS_A else (Z, H, M, K), dtype=DTYPE, device='cuda')
+  b = torch.randn((Z, H, N, K) if TRANS_B else (Z, H, K, N), dtype=DTYPE, device='cuda')
+  shape = {'sdd': (M, N), 'dsd': (a.shape[2], a.shape[3]), 'dds': (b.shape[2], b.shape[3])}[MODE]
   layout = torch.randint(2, (H, shape[0]//BLOCK, shape[1]//BLOCK))
   # triton result
-  lhs = dense_to_sparse(x, layout, BLOCK) if MODE == 'dsd' else x
-  rhs = dense_to_sparse(w, layout, BLOCK) if MODE == 'dds' else w
   op = tt.ops.blocksparse_matmul(layout, BLOCK, MODE, trans_a=TRANS_A, trans_b=TRANS_B)
-  ry  = op(lhs, rhs)
+  ra = sparsify_tensor(a, layout, BLOCK) if MODE == 'dsd' else a
+  rb = sparsify_tensor(b, layout, BLOCK) if MODE == 'dds' else b
+  rc  = op(ra, rb)
   # torch result
-  lhs = sparse_to_dense(x, layout, BLOCK) if MODE == 'dsd' else x
-  rhs = sparse_to_dense(w, layout, BLOCK) if MODE == 'dds' else w
-  lhs = lhs.transpose(2, 3) if TRANS_A else lhs
-  rhs = rhs.transpose(2, 3) if TRANS_B else rhs
-  ty = torch.matmul(lhs, rhs)
-  ty = sparse_to_dense(ty, layout, BLOCK) if MODE == 'sdd' else ty
-  ty = dense_to_sparse(ty, layout, BLOCK) if MODE == 'sdd' else ty
+  ta = mask_tensor(a, layout, BLOCK) if MODE == 'dsd' else a
+  tb = mask_tensor(b, layout, BLOCK) if MODE == 'dds' else b
+  ta = ta.transpose(2, 3) if TRANS_A else ta
+  tb = tb.transpose(2, 3) if TRANS_B else tb
+  tc = torch.matmul(ta, tb)
+  tc = mask_tensor(tc, layout, BLOCK) if MODE == 'sdd' else tc
+  tc = sparsify_tensor(tc, layout, BLOCK) if MODE == 'sdd' else tc
   # compare
   rtol, atol = {torch.float32: (1e-4, 1e-5),
                 torch.float16: (1e-2, 1e-3)}[DTYPE]
-  assert torch.allclose(ry, ty, rtol=rtol, atol=atol)
+  assert torch.allclose(rc, tc, rtol=rtol, atol=atol)
